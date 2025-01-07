Mirantis/hmc

Make provider support more modular

pkg/providers/aws/provider.go
@@ -1,0 +1,63 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aws
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/Mirantis/hmc/pkg/credspropagation"
+)
+
+// Tier-1 provider, always registered.
+
+type Provider struct{}
+
+func (*Provider) GetName() string {
+	return "aws"
+}
+
+func (*Provider) GetTitleName() string {
+	return "AWS"
+}
+
+func (*Provider) GetClusterGVK() schema.GroupVersionKind {
+	return schema.GroupVersionKind{
+		Group:   "infrastructure.cluster.x-k8s.io",
+		Version: "v1beta2",
+		Kind:    "AWSCluster",
+	}
+}
+
+func (*Provider) GetClusterIdentityKinds() []string {
+	return []string{"AWSClusterStaticIdentity", "AWSClusterRoleIdentity", "AWSClusterControllerIdentity"}
+}
+
+func (p *Provider) CredentialPropagationFunc() func(
+	ctx context.Context,
+	propnCfg *credspropagation.PropagationCfg,
+	l logr.Logger,
+) (enabled bool, err error) {
+	return func(
+		_ context.Context,
+		_ *credspropagation.PropagationCfg,
+		l logr.Logger,
+	) (enabled bool, err error) {
+		l.Info("Skipping creds propagation for " + p.GetTitleName())
+		return enabled, err
+	}
+}

pkg/providers/azure/provider.go
@@ -1,0 +1,69 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package azure
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/Mirantis/hmc/pkg/credspropagation"
+	"github.com/Mirantis/hmc/pkg/providers"
+)
+
+type Provider struct{}
+
+var _ providers.ProviderModule = (*Provider)(nil)
+
+func init() {
+	providers.Register(&Provider{})
+}
+
+func (*Provider) GetName() string {
+	return "azure"
+}
+
+func (*Provider) GetTitleName() string {
+	return "Azure"
+}
+
+func (*Provider) GetClusterGVK() schema.GroupVersionKind {
+	return schema.GroupVersionKind{
+		Group:   "infrastructure.cluster.x-k8s.io",
+		Version: "v1beta1",
+		Kind:    "AzureCluster",
+	}
+}
+
+func (*Provider) GetClusterIdentityKinds() []string {
+	return []string{"AzureClusterIdentity"}
+}
+
+func (p *Provider) CredentialPropagationFunc() func(
+	ctx context.Context,
+	propnCfg *credspropagation.PropagationCfg,
+	l logr.Logger,
+) (enabled bool, err error) {
+	return func(
+		ctx context.Context,
+		propnCfg *credspropagation.PropagationCfg,
+		l logr.Logger,
+	) (enabled bool, err error) {
+		l.Info(p.GetTitleName() + " creds propagation start")
+		enabled, err = true, PropagateAzureSecrets(ctx, propnCfg)
+		return enabled, err
+	}
+}

pkg/providers/providers.go
@@ -1,0 +1,164 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package providers
+
+import (
+	"context"
+	"fmt"
+	"slices"
+	"strings"
+	"sync"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	hmc "github.com/Mirantis/hmc/api/v1alpha1"
+	"github.com/Mirantis/hmc/pkg/credspropagation"
+	"github.com/Mirantis/hmc/pkg/providers/aws"
+)
+
+const (
+	// InfraPrefix is the prefix used for infrastructure provider names
+	InfraPrefix = "infrastructure-"
+	// ProviderPrefix is the prefix used for cluster API provider names
+	ProviderPrefix = "cluster-api-provider-"
+)
+
+var (
+	mu sync.RWMutex
+
+	providers = []hmc.Provider{
+		{
+			Name: hmc.ProviderK0smotronName,
+		},
+		{
+			Name: hmc.ProviderSveltosName,
+		},
+	}
+
+	registry map[string]ProviderModule
+)
+
+type ProviderModule interface {
+	// GetName returns the short name of the provider
+	GetName() string
+	// GetTitleName returns the display title of the provider
+	GetTitleName() string
+	// GetClusterGVK returns the GroupVersionKind for the provider's cluster resource
+	GetClusterGVK() schema.GroupVersionKind
+	// GetClusterIdentityKinds returns a list of supported cluster identity kinds
+	GetClusterIdentityKinds() []string
+	// CredentialPropagationFunc returns a function to handle credential propagation
+	CredentialPropagationFunc() func(
+		ctx context.Context,
+		propnCfg *credspropagation.PropagationCfg,
+		l logr.Logger,
+	) (enabled bool, err error)
+}
+
+// Register adds a new provider module to the registry
+func Register(p ProviderModule) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if registry == nil {
+		registry = make(map[string]ProviderModule)
+	}
+
+	shortName := p.GetName()
+
+	if _, exists := registry[shortName]; exists {
+		panic(fmt.Sprintf("provider %q already registered", shortName))
+	}
+
+	providers = append(providers,
+		hmc.Provider{
+			Name: ProviderPrefix + p.GetName(),
+		},
+	)
+
+	registry[shortName] = p
+}
+
+// List returns a copy of all registered providers
+func List() []hmc.Provider {
+	return slices.Clone(providers)
+}
+
+// CredentialPropagationFunc returns the credential propagation function for a given provider
+func CredentialPropagationFunc(fullName string) (
+	func(ctx context.Context, propnCfg *credspropagation.PropagationCfg, l logr.Logger) (enabled bool, err error), bool,
+) {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	shortName := strings.TrimPrefix(fullName, ProviderPrefix)
+
+	module, ok := registry[shortName]
+	if !ok {
+		return nil, false
+	}
+
+	f := module.CredentialPropagationFunc()
+
+	return f, f != nil
+}
+
+// GetClusterGVK returns the GroupVersionKind for a provider's cluster resource
+func GetClusterGVK(shortName string) schema.GroupVersionKind {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	module, ok := registry[shortName]
+	if !ok {
+		return schema.GroupVersionKind{}
+	}
+
+	return module.GetClusterGVK()
+}
+
+// GetClusterIdentityKind returns the supported identity kinds for a given infrastructure provider
+func GetClusterIdentityKind(infraName string) ([]string, bool) {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	shortName := strings.TrimPrefix(infraName, InfraPrefix)
+
+	module, ok := registry[shortName]
+	if !ok {
+		return nil, false
+	}
+
+	list := slices.Clone(module.GetClusterIdentityKinds())
+
+	return list, list != nil
+}
+
+// GetProviderTitleName returns the display title for a given provider
+func GetProviderTitleName(shortName string) string {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	module, ok := registry[shortName]
+	if !ok {
+		return ""
+	}
+
+	return module.GetTitleName()
+}
+
+func init() {
+	Register(&aws.Provider{})
+}

pkg/providers/vsphere/creds.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package credspropagation
+package vsphere
 
 import (
 	"bytes"
@@ -28,9 +28,10 @@ import (
 	"sigs.k8s.io/yaml"
 
 	hmc "github.com/Mirantis/hmc/api/v1alpha1"
+	"github.com/Mirantis/hmc/pkg/credspropagation"
 )
 
-func PropagateVSphereSecrets(ctx context.Context, cfg *PropagationCfg) error {
+func PropagateSecrets(ctx context.Context, cfg *credspropagation.PropagationCfg) error {
 	vsphereCluster := &capv.VSphereCluster{}
 	if err := cfg.Client.Get(ctx, client.ObjectKey{
 		Name:      cfg.ManagedCluster.Name,
@@ -77,7 +78,7 @@ func PropagateVSphereSecrets(ctx context.Context, cfg *PropagationCfg) error {
 		return fmt.Errorf("failed to generate VSphere CSI secret: %w", err)
 	}
 
-	if err := applyCCMConfigs(ctx, cfg.KubeconfSecret, ccmSecret, ccmConfig, csiSecret); err != nil {
+	if err := credspropagation.ApplyCCMConfigs(ctx, cfg.KubeconfSecret, ccmSecret, ccmConfig, csiSecret); err != nil {
 		return fmt.Errorf("failed to apply VSphere CCM/CSI secrets: %w", err)
 	}
 
@@ -119,8 +120,8 @@ func generateVSphereCCMConfigs(vCl *capv.VSphereCluster, vScrt *corev1.Secret, v
 	cmData := map[string]string{
 		"vsphere.conf": string(ccmCfgYaml),
 	}
-	return makeSecret(secretName, metav1.NamespaceSystem, secretData),
-		makeConfigMap("cloud-config", metav1.NamespaceSystem, cmData),
+	return credspropagation.MakeSecret(secretName, metav1.NamespaceSystem, secretData),
+		credspropagation.MakeConfigMap("cloud-config", metav1.NamespaceSystem, cmData),
 		nil
 }
 
@@ -161,5 +162,5 @@ datacenters = "{{ .Datacenter }}"
 		"csi-vsphere.conf": buf.Bytes(),
 	}
 
-	return makeSecret("vcenter-config-secret", metav1.NamespaceSystem, secretData), nil
+	return credspropagation.MakeSecret("vcenter-config-secret", metav1.NamespaceSystem, secretData), nil
 }

pkg/providers/vsphere/provider.go
@@ -1,0 +1,65 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package vsphere
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/Mirantis/hmc/pkg/credspropagation"
+	"github.com/Mirantis/hmc/pkg/providers"
+)
+
+type Provider struct{}
+
+var _ providers.ProviderModule = (*Provider)(nil)
+
+func init() {
+	providers.Register(&Provider{})
+}
+
+func (*Provider) GetName() string {
+	return "vsphere"
+}
+
+func (*Provider) GetTitleName() string {
+	return "vSphere"
+}
+
+func (*Provider) GetClusterGVK() schema.GroupVersionKind {
+	return schema.GroupVersionKind{}
+}
+
+func (*Provider) GetClusterIdentityKinds() []string {
+	return []string{"VSphereClusterIdentity"}
+}
+
+func (p *Provider) CredentialPropagationFunc() func(
+	ctx context.Context,
+	propnCfg *credspropagation.PropagationCfg,
+	l logr.Logger,
+) (enabled bool, err error) {
+	return func(
+		ctx context.Context,
+		propnCfg *credspropagation.PropagationCfg,
+		l logr.Logger,
+	) (enabled bool, err error) {
+		l.Info(p.GetTitleName() + " creds propagation start")
+		enabled, err = true, PropagateSecrets(ctx, propnCfg)
+		return enabled, err
+	}
+}

k0rdent/kcm

pkg/providers/aws/provider.go
@@ -1,0 +1,63 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aws
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/K0rdent/kcm/pkg/credspropagation"
+)
+
+// Tier-1 provider, always registered.
+
+type Provider struct{}
+
+func (*Provider) GetName() string {
+	return "aws"
+}
+
+func (*Provider) GetTitleName() string {
+	return "AWS"
+}
+
+func (*Provider) GetClusterGVK() schema.GroupVersionKind {
+	return schema.GroupVersionKind{
+		Group:   "infrastructure.cluster.x-k8s.io",
+		Version: "v1beta2",
+		Kind:    "AWSCluster",
+	}
+}
+
+func (*Provider) GetClusterIdentityKinds() []string {
+	return []string{"AWSClusterStaticIdentity", "AWSClusterRoleIdentity", "AWSClusterControllerIdentity"}
+}
+
+func (p *Provider) CredentialPropagationFunc() func(
+	ctx context.Context,
+	propnCfg *credspropagation.PropagationCfg,
+	l logr.Logger,
+) (enabled bool, err error) {
+	return func(
+		_ context.Context,
+		_ *credspropagation.PropagationCfg,
+		l logr.Logger,
+	) (enabled bool, err error) {
+		l.Info("Skipping creds propagation for " + p.GetTitleName())
+		return enabled, err
+	}
+}

pkg/providers/azure/provider.go
@@ -1,0 +1,69 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package azure
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/K0rdent/kcm/pkg/credspropagation"
+	"github.com/K0rdent/kcm/pkg/providers"
+)
+
+type Provider struct{}
+
+var _ providers.ProviderModule = (*Provider)(nil)
+
+func init() {
+	providers.Register(&Provider{})
+}
+
+func (*Provider) GetName() string {
+	return "azure"
+}
+
+func (*Provider) GetTitleName() string {
+	return "Azure"
+}
+
+func (*Provider) GetClusterGVK() schema.GroupVersionKind {
+	return schema.GroupVersionKind{
+		Group:   "infrastructure.cluster.x-k8s.io",
+		Version: "v1beta1",
+		Kind:    "AzureCluster",
+	}
+}
+
+func (*Provider) GetClusterIdentityKinds() []string {
+	return []string{"AzureClusterIdentity"}
+}
+
+func (p *Provider) CredentialPropagationFunc() func(
+	ctx context.Context,
+	propnCfg *credspropagation.PropagationCfg,
+	l logr.Logger,
+) (enabled bool, err error) {
+	return func(
+		ctx context.Context,
+		propnCfg *credspropagation.PropagationCfg,
+		l logr.Logger,
+	) (enabled bool, err error) {
+		l.Info(p.GetTitleName() + " creds propagation start")
+		enabled, err = true, PropagateAzureSecrets(ctx, propnCfg)
+		return enabled, err
+	}
+}

pkg/providers/providers.go
@@ -1,0 +1,164 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package providers
+
+import (
+	"context"
+	"fmt"
+	"slices"
+	"strings"
+	"sync"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	hmc "github.com/K0rdent/kcm/api/v1alpha1"
+	"github.com/K0rdent/kcm/pkg/credspropagation"
+	"github.com/K0rdent/kcm/pkg/providers/aws"
+)
+
+const (
+	// InfraPrefix is the prefix used for infrastructure provider names
+	InfraPrefix = "infrastructure-"
+	// ProviderPrefix is the prefix used for cluster API provider names
+	ProviderPrefix = "cluster-api-provider-"
+)
+
+var (
+	mu sync.RWMutex
+
+	providers = []hmc.Provider{
+		{
+			Name: hmc.ProviderK0smotronName,
+		},
+		{
+			Name: hmc.ProviderSveltosName,
+		},
+	}
+
+	registry map[string]ProviderModule
+)
+
+type ProviderModule interface {
+	// GetName returns the short name of the provider
+	GetName() string
+	// GetTitleName returns the display title of the provider
+	GetTitleName() string
+	// GetClusterGVK returns the GroupVersionKind for the provider's cluster resource
+	GetClusterGVK() schema.GroupVersionKind
+	// GetClusterIdentityKinds returns a list of supported cluster identity kinds
+	GetClusterIdentityKinds() []string
+	// CredentialPropagationFunc returns a function to handle credential propagation
+	CredentialPropagationFunc() func(
+		ctx context.Context,
+		propnCfg *credspropagation.PropagationCfg,
+		l logr.Logger,
+	) (enabled bool, err error)
+}
+
+// Register adds a new provider module to the registry
+func Register(p ProviderModule) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if registry == nil {
+		registry = make(map[string]ProviderModule)
+	}
+
+	shortName := p.GetName()
+
+	if _, exists := registry[shortName]; exists {
+		panic(fmt.Sprintf("provider %q already registered", shortName))
+	}
+
+	providers = append(providers,
+		hmc.Provider{
+			Name: ProviderPrefix + p.GetName(),
+		},
+	)
+
+	registry[shortName] = p
+}
+
+// List returns a copy of all registered providers
+func List() []hmc.Provider {
+	return slices.Clone(providers)
+}
+
+// CredentialPropagationFunc returns the credential propagation function for a given provider
+func CredentialPropagationFunc(fullName string) (
+	func(ctx context.Context, propnCfg *credspropagation.PropagationCfg, l logr.Logger) (enabled bool, err error), bool,
+) {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	shortName := strings.TrimPrefix(fullName, ProviderPrefix)
+
+	module, ok := registry[shortName]
+	if !ok {
+		return nil, false
+	}
+
+	f := module.CredentialPropagationFunc()
+
+	return f, f != nil
+}
+
+// GetClusterGVK returns the GroupVersionKind for a provider's cluster resource
+func GetClusterGVK(shortName string) schema.GroupVersionKind {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	module, ok := registry[shortName]
+	if !ok {
+		return schema.GroupVersionKind{}
+	}
+
+	return module.GetClusterGVK()
+}
+
+// GetClusterIdentityKind returns the supported identity kinds for a given infrastructure provider
+func GetClusterIdentityKind(infraName string) ([]string, bool) {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	shortName := strings.TrimPrefix(infraName, InfraPrefix)
+
+	module, ok := registry[shortName]
+	if !ok {
+		return nil, false
+	}
+
+	list := slices.Clone(module.GetClusterIdentityKinds())
+
+	return list, list != nil
+}
+
+// GetProviderTitleName returns the display title for a given provider
+func GetProviderTitleName(shortName string) string {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	module, ok := registry[shortName]
+	if !ok {
+		return ""
+	}
+
+	return module.GetTitleName()
+}
+
+func init() {
+	Register(&aws.Provider{})
+}

pkg/providers/vsphere/creds.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package credspropagation
+package vsphere
 
 import (
 	"bytes"
@@ -28,9 +28,10 @@ import (
 	"sigs.k8s.io/yaml"
 
 	hmc "github.com/K0rdent/kcm/api/v1alpha1"
+	"github.com/K0rdent/kcm/pkg/credspropagation"
 )
 
-func PropagateVSphereSecrets(ctx context.Context, cfg *PropagationCfg) error {
+func PropagateSecrets(ctx context.Context, cfg *credspropagation.PropagationCfg) error {
 	vsphereCluster := &capv.VSphereCluster{}
 	if err := cfg.Client.Get(ctx, client.ObjectKey{
 		Name:      cfg.ClusterDeployment.Name,
@@ -77,7 +78,7 @@ func PropagateVSphereSecrets(ctx context.Context, cfg *PropagationCfg) error {
 		return fmt.Errorf("failed to generate VSphere CSI secret: %w", err)
 	}
 
-	if err := applyCCMConfigs(ctx, cfg.KubeconfSecret, ccmSecret, ccmConfig, csiSecret); err != nil {
+	if err := credspropagation.ApplyCCMConfigs(ctx, cfg.KubeconfSecret, ccmSecret, ccmConfig, csiSecret); err != nil {
 		return fmt.Errorf("failed to apply VSphere CCM/CSI secrets: %w", err)
 	}
 
@@ -119,8 +120,8 @@ func generateVSphereCCMConfigs(vCl *capv.VSphereCluster, vScrt *corev1.Secret, v
 	cmData := map[string]string{
 		"vsphere.conf": string(ccmCfgYaml),
 	}
-	return makeSecret(secretName, secretData),
-		makeConfigMap("cloud-config", cmData),
+	return credspropagation.MakeSecret(secretName, secretData),
+		credspropagation.MakeConfigMap("cloud-config", cmData),
 		nil
 }
 
@@ -161,5 +162,5 @@ datacenters = "{{ .Datacenter }}"
 		"csi-vsphere.conf": buf.Bytes(),
 	}
 
-	return makeSecret("vcenter-config-secret", secretData), nil
+	return credspropagation.MakeSecret("vcenter-config-secret", secretData), nil
 }

pkg/providers/vsphere/provider.go
@@ -1,0 +1,65 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package vsphere
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/K0rdent/kcm/pkg/credspropagation"
+	"github.com/K0rdent/kcm/pkg/providers"
+)
+
+type Provider struct{}
+
+var _ providers.ProviderModule = (*Provider)(nil)
+
+func init() {
+	providers.Register(&Provider{})
+}
+
+func (*Provider) GetName() string {
+	return "vsphere"
+}
+
+func (*Provider) GetTitleName() string {
+	return "vSphere"
+}
+
+func (*Provider) GetClusterGVK() schema.GroupVersionKind {
+	return schema.GroupVersionKind{}
+}
+
+func (*Provider) GetClusterIdentityKinds() []string {
+	return []string{"VSphereClusterIdentity"}
+}
+
+func (p *Provider) CredentialPropagationFunc() func(
+	ctx context.Context,
+	propnCfg *credspropagation.PropagationCfg,
+	l logr.Logger,
+) (enabled bool, err error) {
+	return func(
+		ctx context.Context,
+		propnCfg *credspropagation.PropagationCfg,
+		l logr.Logger,
+	) (enabled bool, err error) {
+		l.Info(p.GetTitleName() + " creds propagation start")
+		enabled, err = true, PropagateSecrets(ctx, propnCfg)
+		return enabled, err
+	}
+}