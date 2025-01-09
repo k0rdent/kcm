Mirantis/hmc

Implement adopted cluster e2e tests

test/e2e/clusterdeployment/clusterdeployment.go
@@ -15,6 +15,7 @@
 package clusterdeployment
 
 import (
+	"context"
 	_ "embed"
 	"fmt"
 	"os"
@@ -27,6 +28,7 @@ import (
 	"gopkg.in/yaml.v3"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
+	"github.com/Mirantis/hmc/test/e2e/kubeclient"
 	"github.com/Mirantis/hmc/test/utils"
 )
 
@@ -37,8 +39,8 @@ const (
 	ProviderAWS     ProviderType = "infrastructure-aws"
 	ProviderAzure   ProviderType = "infrastructure-azure"
 	ProviderVSphere ProviderType = "infrastructure-vsphere"
-
-	providerLabel = "cluster.x-k8s.io/provider"
+	ProviderAdopted ProviderType = "infrastructure-internal"
+	providerLabel                = "cluster.x-k8s.io/provider"
 )
 
 type Template string
@@ -50,6 +52,7 @@ const (
 	TemplateAzureStandaloneCP   Template = "azure-standalone-cp"
 	TemplateVSphereStandaloneCP Template = "vsphere-standalone-cp"
 	TemplateVSphereHostedCP     Template = "vsphere-hosted-cp"
+	TemplateAdoptedCluster      Template = "adopted-cluster"
 )
 
 //go:embed resources/aws-standalone-cp.yaml.tpl
@@ -69,6 +72,9 @@ var vsphereStandaloneCPClusterDeploymentTemplateBytes []byte
 
 //go:embed resources/vsphere-hosted-cp.yaml.tpl
 var vsphereHostedCPClusterDeploymentTemplateBytes []byte
+
+//go:embed resources/adopted-cluster.yaml.tpl
+var adoptedClusterDeploymentTemplateBytes []byte
 
 func FilterAllProviders() []string {
 	return []string{
@@ -134,6 +140,8 @@ func GetUnstructured(templateName Template) *unstructured.Unstructured {
 		clusterDeploymentTemplateBytes = azureHostedCPClusterDeploymentTemplateBytes
 	case TemplateAzureStandaloneCP:
 		clusterDeploymentTemplateBytes = azureStandaloneCPClusterDeploymentTemplateBytes
+	case TemplateAdoptedCluster:
+		clusterDeploymentTemplateBytes = adoptedClusterDeploymentTemplateBytes
 	default:
 		Fail(fmt.Sprintf("Unsupported template: %s", templateName))
 	}
@@ -155,4 +163,28 @@ func ValidateDeploymentVars(v []string) {
 	for _, envVar := range v {
 		Expect(os.Getenv(envVar)).NotTo(BeEmpty(), envVar+" must be set")
 	}
+}
+
+func ValidateClusterTemplates(ctx context.Context, client *kubeclient.KubeClient) error {
+	templates, err := client.ListClusterTemplates(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to list cluster templates: %w", err)
+	}
+
+	for _, template := range templates {
+		valid, found, err := unstructured.NestedBool(template.Object, "status", "valid")
+		if err != nil {
+			return fmt.Errorf("failed to get valid flag for template %s: %w", template.GetName(), err)
+		}
+
+		if !found {
+			return fmt.Errorf("valid flag for template %s not found", template.GetName())
+		}
+
+		if !valid {
+			return fmt.Errorf("template %s is still invalid", template.GetName())
+		}
+	}
+
+	return nil
 }

test/e2e/provider_adopted_test.go
@@ -1,0 +1,133 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package e2e
+
+import (
+	"context"
+	"os"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	internalutils "github.com/Mirantis/hmc/internal/utils"
+	"github.com/Mirantis/hmc/test/e2e/clusterdeployment"
+	"github.com/Mirantis/hmc/test/e2e/clusterdeployment/clusteridentity"
+	"github.com/Mirantis/hmc/test/e2e/kubeclient"
+)
+
+var _ = Describe("Adopted Cluster Templates", Label("provider:cloud", "provider:adopted"), Ordered, func() {
+	var (
+		kc                *kubeclient.KubeClient
+		standaloneClient  *kubeclient.KubeClient
+		clusterDeleteFunc func() error
+		adoptedDeleteFunc func() error
+		kubecfgDeleteFunc func() error
+		clusterName       string
+	)
+
+	BeforeAll(func() {
+		By("providing cluster identity")
+		kc = kubeclient.NewFromLocal(internalutils.DefaultSystemNamespace)
+		ci := clusteridentity.New(kc, clusterdeployment.ProviderAWS)
+		Expect(os.Setenv(clusterdeployment.EnvVarAWSClusterIdentity, ci.IdentityName)).Should(Succeed())
+		ci.WaitForValidCredential(kc)
+	})
+
+	AfterAll(func() {
+		// If we failed collect logs from each of the affiliated controllers
+		// as well as the output of clusterctl to store as artifacts.
+		if CurrentSpecReport().Failed() && !noCleanup() {
+			if standaloneClient != nil {
+				By("collecting failure logs from hosted controllers")
+				collectLogArtifacts(standaloneClient, clusterName, clusterdeployment.ProviderAWS, clusterdeployment.ProviderCAPI)
+			}
+		}
+
+		By("deleting resources")
+		for _, deleteFunc := range []func() error{
+			kubecfgDeleteFunc,
+			adoptedDeleteFunc,
+			clusterDeleteFunc,
+		} {
+			if deleteFunc != nil {
+				err := deleteFunc()
+				Expect(err).NotTo(HaveOccurred())
+			}
+		}
+	})
+
+	It("should work with an Adopted cluster provider", func() {
+		// Deploy a standalone cluster and verify it is running/ready. Then, delete the management cluster and
+		// recreate it. Next "adopt" the cluster we created and verify the services were deployed.
+		GinkgoT().Setenv(clusterdeployment.EnvVarAWSInstanceType, "t3.xlarge")
+
+		templateBy(clusterdeployment.TemplateAWSStandaloneCP, "creating a ManagedCluster")
+		sd := clusterdeployment.GetUnstructured(clusterdeployment.TemplateAWSStandaloneCP)
+		clusterName = sd.GetName()
+
+		clusterDeleteFunc = kc.CreateClusterDeployment(context.Background(), sd)
+
+		templateBy(clusterdeployment.TemplateAWSStandaloneCP, "waiting for infrastructure to deploy successfully")
+		deploymentValidator := clusterdeployment.NewProviderValidator(
+			clusterdeployment.TemplateAWSStandaloneCP,
+			clusterName,
+			clusterdeployment.ValidationActionDeploy,
+		)
+
+		Eventually(func() error {
+			return deploymentValidator.Validate(context.Background(), kc)
+		}).WithTimeout(30 * time.Minute).WithPolling(10 * time.Second).Should(Succeed())
+
+		// create the adopted cluster using the AWS standalone cluster
+		var kubeCfgFile string
+		kubeCfgFile, kubecfgDeleteFunc = kc.WriteKubeconfig(context.Background(), clusterName)
+		GinkgoT().Setenv(clusterdeployment.EnvVarAdoptedKubeconfigPath, kubeCfgFile)
+		ci := clusteridentity.New(kc, clusterdeployment.ProviderAdopted)
+		Expect(os.Setenv(clusterdeployment.EnvVarAdoptedCredential, ci.CredentialName)).Should(Succeed())
+
+		ci.WaitForValidCredential(kc)
+
+		adoptedCluster := clusterdeployment.GetUnstructured(clusterdeployment.TemplateAdoptedCluster)
+		adoptedClusterName := adoptedCluster.GetName()
+		adoptedDeleteFunc = kc.CreateClusterDeployment(context.Background(), adoptedCluster)
+
+		// validate the adopted cluster
+		deploymentValidator = clusterdeployment.NewProviderValidator(
+			clusterdeployment.TemplateAdoptedCluster,
+			adoptedClusterName,
+			clusterdeployment.ValidationActionDeploy,
+		)
+		Eventually(func() error {
+			return deploymentValidator.Validate(context.Background(), kc)
+		}).WithTimeout(30 * time.Minute).WithPolling(10 * time.Second).Should(Succeed())
+
+		err := adoptedDeleteFunc()
+		Expect(err).NotTo(HaveOccurred())
+
+		err = clusterDeleteFunc()
+		Expect(err).NotTo(HaveOccurred())
+
+		// finally delete the aws standalone clsuter
+		deletionValidator := clusterdeployment.NewProviderValidator(
+			clusterdeployment.TemplateAWSStandaloneCP,
+			clusterName,
+			clusterdeployment.ValidationActionDelete,
+		)
+		Eventually(func() error {
+			return deletionValidator.Validate(context.Background(), kc)
+		}).WithTimeout(30 * time.Minute).WithPolling(10 * time.Second).Should(Succeed())
+	})
+})

k0rdent/kcm

test/e2e/clusterdeployment/clusterdeployment.go
@@ -15,6 +15,7 @@
 package clusterdeployment
 
 import (
+	"context"
 	_ "embed"
 	"fmt"
 	"os"
@@ -27,6 +28,7 @@ import (
 	"gopkg.in/yaml.v3"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
+	"github.com/K0rdent/kcm/test/e2e/kubeclient"
 	"github.com/K0rdent/kcm/test/utils"
 )
 
@@ -37,8 +39,8 @@ const (
 	ProviderAWS     ProviderType = "infrastructure-aws"
 	ProviderAzure   ProviderType = "infrastructure-azure"
 	ProviderVSphere ProviderType = "infrastructure-vsphere"
-
-	providerLabel = "cluster.x-k8s.io/provider"
+	ProviderAdopted ProviderType = "infrastructure-internal"
+	providerLabel                = "cluster.x-k8s.io/provider"
 )
 
 type Template string
@@ -50,6 +52,7 @@ const (
 	TemplateAzureStandaloneCP   Template = "azure-standalone-cp"
 	TemplateVSphereStandaloneCP Template = "vsphere-standalone-cp"
 	TemplateVSphereHostedCP     Template = "vsphere-hosted-cp"
+	TemplateAdoptedCluster      Template = "adopted-cluster"
 )
 
 //go:embed resources/aws-standalone-cp.yaml.tpl
@@ -69,6 +72,9 @@ var vsphereStandaloneCPClusterDeploymentTemplateBytes []byte
 
 //go:embed resources/vsphere-hosted-cp.yaml.tpl
 var vsphereHostedCPClusterDeploymentTemplateBytes []byte
+
+//go:embed resources/adopted-cluster.yaml.tpl
+var adoptedClusterDeploymentTemplateBytes []byte
 
 func FilterAllProviders() []string {
 	return []string{
@@ -134,6 +140,8 @@ func GetUnstructured(templateName Template) *unstructured.Unstructured {
 		clusterDeploymentTemplateBytes = azureHostedCPClusterDeploymentTemplateBytes
 	case TemplateAzureStandaloneCP:
 		clusterDeploymentTemplateBytes = azureStandaloneCPClusterDeploymentTemplateBytes
+	case TemplateAdoptedCluster:
+		clusterDeploymentTemplateBytes = adoptedClusterDeploymentTemplateBytes
 	default:
 		Fail(fmt.Sprintf("Unsupported template: %s", templateName))
 	}
@@ -155,4 +163,28 @@ func ValidateDeploymentVars(v []string) {
 	for _, envVar := range v {
 		Expect(os.Getenv(envVar)).NotTo(BeEmpty(), envVar+" must be set")
 	}
+}
+
+func ValidateClusterTemplates(ctx context.Context, client *kubeclient.KubeClient) error {
+	templates, err := client.ListClusterTemplates(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to list cluster templates: %w", err)
+	}
+
+	for _, template := range templates {
+		valid, found, err := unstructured.NestedBool(template.Object, "status", "valid")
+		if err != nil {
+			return fmt.Errorf("failed to get valid flag for template %s: %w", template.GetName(), err)
+		}
+
+		if !found {
+			return fmt.Errorf("valid flag for template %s not found", template.GetName())
+		}
+
+		if !valid {
+			return fmt.Errorf("template %s is still invalid", template.GetName())
+		}
+	}
+
+	return nil
 }

test/e2e/provider_adopted_test.go
@@ -1,0 +1,133 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package e2e
+
+import (
+	"context"
+	"os"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	internalutils "github.com/K0rdent/kcm/internal/utils"
+	"github.com/K0rdent/kcm/test/e2e/clusterdeployment"
+	"github.com/K0rdent/kcm/test/e2e/clusterdeployment/clusteridentity"
+	"github.com/K0rdent/kcm/test/e2e/kubeclient"
+)
+
+var _ = Describe("Adopted Cluster Templates", Label("provider:cloud", "provider:adopted"), Ordered, func() {
+	var (
+		kc                *kubeclient.KubeClient
+		standaloneClient  *kubeclient.KubeClient
+		clusterDeleteFunc func() error
+		adoptedDeleteFunc func() error
+		kubecfgDeleteFunc func() error
+		clusterName       string
+	)
+
+	BeforeAll(func() {
+		By("providing cluster identity")
+		kc = kubeclient.NewFromLocal(internalutils.DefaultSystemNamespace)
+		ci := clusteridentity.New(kc, clusterdeployment.ProviderAWS)
+		Expect(os.Setenv(clusterdeployment.EnvVarAWSClusterIdentity, ci.IdentityName)).Should(Succeed())
+		ci.WaitForValidCredential(kc)
+	})
+
+	AfterAll(func() {
+		// If we failed collect logs from each of the affiliated controllers
+		// as well as the output of clusterctl to store as artifacts.
+		if CurrentSpecReport().Failed() && !noCleanup() {
+			if standaloneClient != nil {
+				By("collecting failure logs from hosted controllers")
+				collectLogArtifacts(standaloneClient, clusterName, clusterdeployment.ProviderAWS, clusterdeployment.ProviderCAPI)
+			}
+		}
+
+		By("deleting resources")
+		for _, deleteFunc := range []func() error{
+			kubecfgDeleteFunc,
+			adoptedDeleteFunc,
+			clusterDeleteFunc,
+		} {
+			if deleteFunc != nil {
+				err := deleteFunc()
+				Expect(err).NotTo(HaveOccurred())
+			}
+		}
+	})
+
+	It("should work with an Adopted cluster provider", func() {
+		// Deploy a standalone cluster and verify it is running/ready. Then, delete the management cluster and
+		// recreate it. Next "adopt" the cluster we created and verify the services were deployed.
+		GinkgoT().Setenv(clusterdeployment.EnvVarAWSInstanceType, "t3.xlarge")
+
+		templateBy(clusterdeployment.TemplateAWSStandaloneCP, "creating a ManagedCluster")
+		sd := clusterdeployment.GetUnstructured(clusterdeployment.TemplateAWSStandaloneCP)
+		clusterName = sd.GetName()
+
+		clusterDeleteFunc = kc.CreateClusterDeployment(context.Background(), sd)
+
+		templateBy(clusterdeployment.TemplateAWSStandaloneCP, "waiting for infrastructure to deploy successfully")
+		deploymentValidator := clusterdeployment.NewProviderValidator(
+			clusterdeployment.TemplateAWSStandaloneCP,
+			clusterName,
+			clusterdeployment.ValidationActionDeploy,
+		)
+
+		Eventually(func() error {
+			return deploymentValidator.Validate(context.Background(), kc)
+		}).WithTimeout(30 * time.Minute).WithPolling(10 * time.Second).Should(Succeed())
+
+		// create the adopted cluster using the AWS standalone cluster
+		var kubeCfgFile string
+		kubeCfgFile, kubecfgDeleteFunc = kc.WriteKubeconfig(context.Background(), clusterName)
+		GinkgoT().Setenv(clusterdeployment.EnvVarAdoptedKubeconfigPath, kubeCfgFile)
+		ci := clusteridentity.New(kc, clusterdeployment.ProviderAdopted)
+		Expect(os.Setenv(clusterdeployment.EnvVarAdoptedCredential, ci.CredentialName)).Should(Succeed())
+
+		ci.WaitForValidCredential(kc)
+
+		adoptedCluster := clusterdeployment.GetUnstructured(clusterdeployment.TemplateAdoptedCluster)
+		adoptedClusterName := adoptedCluster.GetName()
+		adoptedDeleteFunc = kc.CreateClusterDeployment(context.Background(), adoptedCluster)
+
+		// validate the adopted cluster
+		deploymentValidator = clusterdeployment.NewProviderValidator(
+			clusterdeployment.TemplateAdoptedCluster,
+			adoptedClusterName,
+			clusterdeployment.ValidationActionDeploy,
+		)
+		Eventually(func() error {
+			return deploymentValidator.Validate(context.Background(), kc)
+		}).WithTimeout(30 * time.Minute).WithPolling(10 * time.Second).Should(Succeed())
+
+		err := adoptedDeleteFunc()
+		Expect(err).NotTo(HaveOccurred())
+
+		err = clusterDeleteFunc()
+		Expect(err).NotTo(HaveOccurred())
+
+		// finally delete the aws standalone clsuter
+		deletionValidator := clusterdeployment.NewProviderValidator(
+			clusterdeployment.TemplateAWSStandaloneCP,
+			clusterName,
+			clusterdeployment.ValidationActionDelete,
+		)
+		Eventually(func() error {
+			return deletionValidator.Validate(context.Background(), kc)
+		}).WithTimeout(30 * time.Minute).WithPolling(10 * time.Second).Should(Succeed())
+	})
+})