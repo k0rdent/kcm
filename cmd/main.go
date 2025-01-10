s3rj1k/hmc

Modular provider support using inline Lua

cmd/main.go
@@ -15,9 +15,12 @@
 package main
 
 import (
+	"cmp"
 	"crypto/tls"
 	"flag"
+	"fmt"
 	"os"
+	"strings"
 
 	hcv2 "github.com/fluxcd/helm-controller/api/v2"
 	sourcev1 "github.com/fluxcd/source-controller/api/v1"
@@ -27,7 +30,6 @@ import (
 	"k8s.io/client-go/dynamic"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
-	capz "sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
 	capv "sigs.k8s.io/cluster-api-provider-vsphere/apis/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
@@ -36,8 +38,10 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	hmcmirantiscomv1alpha1 "github.com/Mirantis/hmc/api/v1alpha1"
+	"github.com/Mirantis/hmc/internal/build"
 	"github.com/Mirantis/hmc/internal/controller"
 	"github.com/Mirantis/hmc/internal/helm"
+	"github.com/Mirantis/hmc/internal/providers"
 	"github.com/Mirantis/hmc/internal/telemetry"
 	"github.com/Mirantis/hmc/internal/utils"
 	hmcwebhook "github.com/Mirantis/hmc/internal/webhook"
@@ -55,12 +59,16 @@ func init() {
 	utilruntime.Must(sourcev1.AddToScheme(scheme))
 	utilruntime.Must(hcv2.AddToScheme(scheme))
 	utilruntime.Must(sveltosv1beta1.AddToScheme(scheme))
-	utilruntime.Must(capz.AddToScheme(scheme))
 	utilruntime.Must(capv.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
 func main() {
+	providersGlob := cmp.Or(os.Getenv("PROVIDERS_PATH_GLOB"), "/opt/providers/*.yml")
+	if err := providers.RegisterProvidersFromGlob(providersGlob); err != nil {
+		panic(fmt.Sprintf("failed to register providers: %v", err))
+	}
+
 	var (
 		metricsAddr               string
 		probeAddr                 string
@@ -107,6 +115,24 @@ func main() {
 		Development: true,
 	}
 	opts.BindFlags(flag.CommandLine)
+
+	flag.Usage = func() {
+		var defaultUsage strings.Builder
+		{
+			oldOutput := flag.CommandLine.Output()
+			flag.CommandLine.SetOutput(&defaultUsage)
+			flag.PrintDefaults()
+			flag.CommandLine.SetOutput(oldOutput)
+		}
+
+		_, _ = fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		_, _ = fmt.Fprint(os.Stderr, defaultUsage.String())
+		_, _ = fmt.Fprintf(os.Stderr, "\nSupported providers:\n")
+		for _, el := range providers.List() {
+			_, _ = fmt.Fprintf(os.Stderr, "  - %s\n", el)
+		}
+		_, _ = fmt.Fprintf(os.Stderr, "\nVersion: %s\n", build.Version)
+	}
 	flag.Parse()
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))

internal/controller/release_controller.go
@@ -47,6 +47,7 @@ import (
 	hmc "github.com/Mirantis/hmc/api/v1alpha1"
 	"github.com/Mirantis/hmc/internal/build"
 	"github.com/Mirantis/hmc/internal/helm"
+	"github.com/Mirantis/hmc/internal/providers"
 	"github.com/Mirantis/hmc/internal/utils"
 )
 
@@ -194,7 +195,7 @@ func (r *ReleaseReconciler) ensureManagement(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	mgmtObj.Spec.Providers = hmc.GetDefaultProviders()
+	mgmtObj.Spec.Providers = providers.List()
 
 	getter := helm.NewMemoryRESTClientGetter(r.Config, r.RESTMapper())
 	actionConfig := new(action.Configuration)

internal/providers/providers.go
@@ -1,0 +1,159 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package providers
+
+import (
+	"context"
+	"fmt"
+	"slices"
+	"strings"
+	"sync"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	hmc "github.com/Mirantis/hmc/api/v1alpha1"
+	"github.com/Mirantis/hmc/internal/credspropagation"
+)
+
+const (
+	// InfraPrefix is the prefix used for infrastructure provider names
+	InfraPrefix = "infrastructure-"
+	// ProviderPrefix is the prefix used for cluster API provider names
+	ProviderPrefix = "cluster-api-provider-"
+)
+
+var (
+	mu sync.RWMutex
+
+	providers = []hmc.Provider{
+		{
+			Name: hmc.ProviderK0smotronName,
+		},
+		{
+			Name: hmc.ProviderSveltosName,
+		},
+	}
+
+	registry map[string]ProviderModule
+)
+
+type ProviderModule interface {
+	// GetName returns the short name of the provider
+	GetName() string
+	// GetTitleName returns the display title of the provider
+	GetTitleName() string
+	// GetClusterGVK returns the GroupVersionKind for the provider's cluster resource
+	GetClusterGVK() schema.GroupVersionKind
+	// GetClusterIdentityKinds returns a list of supported cluster identity kinds
+	GetClusterIdentityKinds() []string
+	// CredentialPropagationFunc returns a function to handle credential propagation
+	CredentialPropagationFunc() func(
+		ctx context.Context,
+		cfg *credspropagation.PropagationCfg,
+		l logr.Logger,
+	) (enabled bool, err error)
+}
+
+// Register adds a new provider module to the registry
+func Register(p ProviderModule) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if registry == nil {
+		registry = make(map[string]ProviderModule)
+	}
+
+	shortName := p.GetName()
+
+	if _, exists := registry[shortName]; exists {
+		panic(fmt.Sprintf("provider %q already registered", shortName))
+	}
+
+	providers = append(providers,
+		hmc.Provider{
+			Name: ProviderPrefix + p.GetName(),
+		},
+	)
+
+	registry[shortName] = p
+}
+
+// List returns a copy of all registered providers
+func List() []hmc.Provider {
+	return slices.Clone(providers)
+}
+
+// CredentialPropagationFunc returns the credential propagation function for a given provider
+func CredentialPropagationFunc(fullName string) (
+	func(ctx context.Context, propnCfg *credspropagation.PropagationCfg, l logr.Logger) (enabled bool, err error), bool,
+) {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	shortName := strings.TrimPrefix(fullName, ProviderPrefix)
+
+	module, ok := registry[shortName]
+	if !ok {
+		return nil, false
+	}
+
+	f := module.CredentialPropagationFunc()
+
+	return f, f != nil
+}
+
+// GetClusterGVK returns the GroupVersionKind for a provider's cluster resource
+func GetClusterGVK(shortName string) schema.GroupVersionKind {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	module, ok := registry[shortName]
+	if !ok {
+		return schema.GroupVersionKind{}
+	}
+
+	return module.GetClusterGVK()
+}
+
+// GetClusterIdentityKind returns the supported identity kinds for a given infrastructure provider
+func GetClusterIdentityKind(infraName string) ([]string, bool) {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	shortName := strings.TrimPrefix(infraName, InfraPrefix)
+
+	module, ok := registry[shortName]
+	if !ok {
+		return nil, false
+	}
+
+	list := slices.Clone(module.GetClusterIdentityKinds())
+
+	return list, list != nil
+}
+
+// GetProviderTitleName returns the display title for a given provider
+func GetProviderTitleName(shortName string) string {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	module, ok := registry[shortName]
+	if !ok {
+		return ""
+	}
+
+	return module.GetTitleName()
+}

internal/providers/vsphere.go
@@ -1,0 +1,64 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package providers
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/Mirantis/hmc/internal/credspropagation"
+)
+
+type ProvidervSphere struct{}
+
+var _ ProviderModule = (*ProvidervSphere)(nil)
+
+func init() {
+	Register(&ProvidervSphere{})
+}
+
+func (*ProvidervSphere) GetName() string {
+	return "vsphere"
+}
+
+func (*ProvidervSphere) GetTitleName() string {
+	return "vSphere"
+}
+
+func (*ProvidervSphere) GetClusterGVK() schema.GroupVersionKind {
+	return schema.GroupVersionKind{}
+}
+
+func (*ProvidervSphere) GetClusterIdentityKinds() []string {
+	return []string{"VSphereClusterIdentity"}
+}
+
+func (p *ProvidervSphere) CredentialPropagationFunc() func(
+	ctx context.Context,
+	cfg *credspropagation.PropagationCfg,
+	l logr.Logger,
+) (enabled bool, err error) {
+	return func(
+		ctx context.Context,
+		cfg *credspropagation.PropagationCfg,
+		l logr.Logger,
+	) (enabled bool, err error) {
+		l.Info(p.GetTitleName() + " creds propagation start")
+		enabled, err = true, credspropagation.PropagateVSphereProviderObjects(ctx, cfg)
+		return enabled, err
+	}
+}

internal/providers/yaml.go
@@ -1,0 +1,123 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package providers
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"slices"
+
+	"github.com/go-logr/logr"
+	"gopkg.in/yaml.v3"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/Mirantis/hmc/internal/credspropagation"
+)
+
+// GVK represents the GroupVersionKind structure in YAML
+type GVK struct {
+	Group   string `yaml:"group"`
+	Version string `yaml:"version"`
+	Kind    string `yaml:"kind"`
+}
+
+// CredentialPropagation represents the credential propagation configuration
+type CredentialPropagation struct {
+	Lua     string `yaml:"lua"`
+	Enabled bool   `yaml:"enabled"`
+}
+
+// YAMLProviderDefinition represents a YAML-based provider configuration
+type YAMLProviderDefinition struct {
+	Name                  string                `yaml:"name"`
+	Title                 string                `yaml:"title"`
+	ClusterGVK            GVK                   `yaml:"clusterGVK"`
+	ClusterIdentityKinds  []string              `yaml:"clusterIdentityKinds"`
+	CredentialPropagation CredentialPropagation `yaml:"credentialPropagation"`
+}
+
+var _ ProviderModule = (*YAMLProviderDefinition)(nil)
+
+func (p *YAMLProviderDefinition) GetName() string {
+	return p.Name
+}
+
+func (p *YAMLProviderDefinition) GetTitleName() string {
+	return p.Title
+}
+
+func (p *YAMLProviderDefinition) GetClusterGVK() schema.GroupVersionKind {
+	return schema.GroupVersionKind{
+		Group:   p.ClusterGVK.Group,
+		Version: p.ClusterGVK.Version,
+		Kind:    p.ClusterGVK.Kind,
+	}
+}
+
+func (p *YAMLProviderDefinition) GetClusterIdentityKinds() []string {
+	return slices.Clone(p.ClusterIdentityKinds)
+}
+
+func (p *YAMLProviderDefinition) CredentialPropagationFunc() func(
+	ctx context.Context,
+	cfg *credspropagation.PropagationCfg,
+	l logr.Logger,
+) (enabled bool, err error) {
+	return func(
+		ctx context.Context,
+		cfg *credspropagation.PropagationCfg,
+		l logr.Logger,
+	) (bool, error) {
+		l.Info(p.GetTitleName() + " creds propagation start")
+		return p.CredentialPropagation.Enabled,
+			credspropagation.PropagateProviderObjects(ctx, cfg, p.CredentialPropagation.Lua)
+	}
+}
+
+// RegisterFromYAML registers a provider from a YAML file
+func RegisterFromYAML(yamlFile string) error {
+	data, err := os.ReadFile(yamlFile)
+	if err != nil {
+		return fmt.Errorf("failed to read YAML file: %w", err)
+	}
+
+	var ypd YAMLProviderDefinition
+
+	if err := yaml.Unmarshal(data, &ypd); err != nil {
+		return fmt.Errorf("failed to unmarshal YAML: %w", err)
+	}
+
+	Register(&ypd)
+
+	return nil
+}
+
+// RegisterProvidersFromGlob loads and registers provider YAML files matching the glob pattern
+func RegisterProvidersFromGlob(pattern string) error {
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		return fmt.Errorf("failed to glob pattern %q: %w", pattern, err)
+	}
+
+	for _, file := range matches {
+		if err := RegisterFromYAML(file); err != nil {
+			return fmt.Errorf("provider %s: %w", filepath.Base(file), err)
+		}
+	}
+
+	return nil
+}

k0rdent/kcm

cmd/main.go
@@ -15,9 +15,12 @@
 package main
 
 import (
+	"cmp"
 	"crypto/tls"
 	"flag"
+	"fmt"
 	"os"
+	"strings"
 
 	hcv2 "github.com/fluxcd/helm-controller/api/v2"
 	sourcev1 "github.com/fluxcd/source-controller/api/v1"
@@ -31,7 +34,6 @@ import (
 	"k8s.io/client-go/dynamic"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
-	capz "sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
 	capo "sigs.k8s.io/cluster-api-provider-openstack/api/v1beta1"
 	capv "sigs.k8s.io/cluster-api-provider-vsphere/apis/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -41,8 +43,10 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	hmcmirantiscomv1alpha1 "github.com/K0rdent/kcm/api/v1alpha1"
+	"github.com/K0rdent/kcm/internal/build"
 	"github.com/K0rdent/kcm/internal/controller"
 	"github.com/K0rdent/kcm/internal/helm"
+	"github.com/K0rdent/kcm/internal/providers"
 	"github.com/K0rdent/kcm/internal/telemetry"
 	"github.com/K0rdent/kcm/internal/utils"
 	hmcwebhook "github.com/K0rdent/kcm/internal/webhook"
@@ -69,13 +73,17 @@ func init() {
 	utilruntime.Must(sourcev1.AddToScheme(scheme))
 	utilruntime.Must(hcv2.AddToScheme(scheme))
 	utilruntime.Must(sveltosv1beta1.AddToScheme(scheme))
-	utilruntime.Must(capz.AddToScheme(scheme))
 	utilruntime.Must(capv.AddToScheme(scheme))
 	utilruntime.Must(capo.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
 func main() {
+	providersGlob := cmp.Or(os.Getenv("PROVIDERS_PATH_GLOB"), "/opt/providers/*.yml")
+	if err := providers.RegisterProvidersFromGlob(providersGlob); err != nil {
+		panic(fmt.Sprintf("failed to register providers: %v", err))
+	}
+
 	var (
 		metricsAddr               string
 		probeAddr                 string
@@ -122,6 +130,24 @@ func main() {
 		Development: true,
 	}
 	opts.BindFlags(flag.CommandLine)
+
+	flag.Usage = func() {
+		var defaultUsage strings.Builder
+		{
+			oldOutput := flag.CommandLine.Output()
+			flag.CommandLine.SetOutput(&defaultUsage)
+			flag.PrintDefaults()
+			flag.CommandLine.SetOutput(oldOutput)
+		}
+
+		_, _ = fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		_, _ = fmt.Fprint(os.Stderr, defaultUsage.String())
+		_, _ = fmt.Fprintf(os.Stderr, "\nSupported providers:\n")
+		for _, el := range providers.List() {
+			_, _ = fmt.Fprintf(os.Stderr, "  - %s\n", el)
+		}
+		_, _ = fmt.Fprintf(os.Stderr, "\nVersion: %s\n", build.Version)
+	}
 	flag.Parse()
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))

internal/controller/release_controller.go
@@ -47,6 +47,7 @@ import (
 	hmc "github.com/K0rdent/kcm/api/v1alpha1"
 	"github.com/K0rdent/kcm/internal/build"
 	"github.com/K0rdent/kcm/internal/helm"
+	"github.com/K0rdent/kcm/internal/providers"
 	"github.com/K0rdent/kcm/internal/utils"
 )
 
@@ -200,7 +201,7 @@ func (r *ReleaseReconciler) ensureManagement(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	mgmtObj.Spec.Providers = hmc.GetDefaultProviders()
+	mgmtObj.Spec.Providers = providers.List()
 
 	getter := helm.NewMemoryRESTClientGetter(r.Config, r.RESTMapper())
 	actionConfig := new(action.Configuration)

internal/providers/providers.go
@@ -1,0 +1,159 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package providers
+
+import (
+	"context"
+	"fmt"
+	"slices"
+	"strings"
+	"sync"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	hmc "github.com/K0rdent/kcm/api/v1alpha1"
+	"github.com/K0rdent/kcm/internal/credspropagation"
+)
+
+const (
+	// InfraPrefix is the prefix used for infrastructure provider names
+	InfraPrefix = "infrastructure-"
+	// ProviderPrefix is the prefix used for cluster API provider names
+	ProviderPrefix = "cluster-api-provider-"
+)
+
+var (
+	mu sync.RWMutex
+
+	providers = []hmc.Provider{
+		{
+			Name: hmc.ProviderK0smotronName,
+		},
+		{
+			Name: hmc.ProviderSveltosName,
+		},
+	}
+
+	registry map[string]ProviderModule
+)
+
+type ProviderModule interface {
+	// GetName returns the short name of the provider
+	GetName() string
+	// GetTitleName returns the display title of the provider
+	GetTitleName() string
+	// GetClusterGVK returns the GroupVersionKind for the provider's cluster resource
+	GetClusterGVK() schema.GroupVersionKind
+	// GetClusterIdentityKinds returns a list of supported cluster identity kinds
+	GetClusterIdentityKinds() []string
+	// CredentialPropagationFunc returns a function to handle credential propagation
+	CredentialPropagationFunc() func(
+		ctx context.Context,
+		cfg *credspropagation.PropagationCfg,
+		l logr.Logger,
+	) (enabled bool, err error)
+}
+
+// Register adds a new provider module to the registry
+func Register(p ProviderModule) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if registry == nil {
+		registry = make(map[string]ProviderModule)
+	}
+
+	shortName := p.GetName()
+
+	if _, exists := registry[shortName]; exists {
+		panic(fmt.Sprintf("provider %q already registered", shortName))
+	}
+
+	providers = append(providers,
+		hmc.Provider{
+			Name: ProviderPrefix + p.GetName(),
+		},
+	)
+
+	registry[shortName] = p
+}
+
+// List returns a copy of all registered providers
+func List() []hmc.Provider {
+	return slices.Clone(providers)
+}
+
+// CredentialPropagationFunc returns the credential propagation function for a given provider
+func CredentialPropagationFunc(fullName string) (
+	func(ctx context.Context, propnCfg *credspropagation.PropagationCfg, l logr.Logger) (enabled bool, err error), bool,
+) {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	shortName := strings.TrimPrefix(fullName, ProviderPrefix)
+
+	module, ok := registry[shortName]
+	if !ok {
+		return nil, false
+	}
+
+	f := module.CredentialPropagationFunc()
+
+	return f, f != nil
+}
+
+// GetClusterGVK returns the GroupVersionKind for a provider's cluster resource
+func GetClusterGVK(shortName string) schema.GroupVersionKind {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	module, ok := registry[shortName]
+	if !ok {
+		return schema.GroupVersionKind{}
+	}
+
+	return module.GetClusterGVK()
+}
+
+// GetClusterIdentityKind returns the supported identity kinds for a given infrastructure provider
+func GetClusterIdentityKind(infraName string) ([]string, bool) {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	shortName := strings.TrimPrefix(infraName, InfraPrefix)
+
+	module, ok := registry[shortName]
+	if !ok {
+		return nil, false
+	}
+
+	list := slices.Clone(module.GetClusterIdentityKinds())
+
+	return list, list != nil
+}
+
+// GetProviderTitleName returns the display title for a given provider
+func GetProviderTitleName(shortName string) string {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	module, ok := registry[shortName]
+	if !ok {
+		return ""
+	}
+
+	return module.GetTitleName()
+}

internal/providers/vsphere.go
@@ -1,0 +1,64 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package providers
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/K0rdent/kcm/internal/credspropagation"
+)
+
+type ProvidervSphere struct{}
+
+var _ ProviderModule = (*ProvidervSphere)(nil)
+
+func init() {
+	Register(&ProvidervSphere{})
+}
+
+func (*ProvidervSphere) GetName() string {
+	return "vsphere"
+}
+
+func (*ProvidervSphere) GetTitleName() string {
+	return "vSphere"
+}
+
+func (*ProvidervSphere) GetClusterGVK() schema.GroupVersionKind {
+	return schema.GroupVersionKind{}
+}
+
+func (*ProvidervSphere) GetClusterIdentityKinds() []string {
+	return []string{"VSphereClusterIdentity"}
+}
+
+func (p *ProvidervSphere) CredentialPropagationFunc() func(
+	ctx context.Context,
+	cfg *credspropagation.PropagationCfg,
+	l logr.Logger,
+) (enabled bool, err error) {
+	return func(
+		ctx context.Context,
+		cfg *credspropagation.PropagationCfg,
+		l logr.Logger,
+	) (enabled bool, err error) {
+		l.Info(p.GetTitleName() + " creds propagation start")
+		enabled, err = true, credspropagation.PropagateVSphereProviderObjects(ctx, cfg)
+		return enabled, err
+	}
+}

internal/providers/yaml.go
@@ -1,0 +1,123 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package providers
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"slices"
+
+	"github.com/go-logr/logr"
+	"gopkg.in/yaml.v3"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/K0rdent/kcm/internal/credspropagation"
+)
+
+// GVK represents the GroupVersionKind structure in YAML
+type GVK struct {
+	Group   string `yaml:"group"`
+	Version string `yaml:"version"`
+	Kind    string `yaml:"kind"`
+}
+
+// CredentialPropagation represents the credential propagation configuration
+type CredentialPropagation struct {
+	Lua     string `yaml:"lua"`
+	Enabled bool   `yaml:"enabled"`
+}
+
+// YAMLProviderDefinition represents a YAML-based provider configuration
+type YAMLProviderDefinition struct {
+	Name                  string                `yaml:"name"`
+	Title                 string                `yaml:"title"`
+	ClusterGVK            GVK                   `yaml:"clusterGVK"`
+	ClusterIdentityKinds  []string              `yaml:"clusterIdentityKinds"`
+	CredentialPropagation CredentialPropagation `yaml:"credentialPropagation"`
+}
+
+var _ ProviderModule = (*YAMLProviderDefinition)(nil)
+
+func (p *YAMLProviderDefinition) GetName() string {
+	return p.Name
+}
+
+func (p *YAMLProviderDefinition) GetTitleName() string {
+	return p.Title
+}
+
+func (p *YAMLProviderDefinition) GetClusterGVK() schema.GroupVersionKind {
+	return schema.GroupVersionKind{
+		Group:   p.ClusterGVK.Group,
+		Version: p.ClusterGVK.Version,
+		Kind:    p.ClusterGVK.Kind,
+	}
+}
+
+func (p *YAMLProviderDefinition) GetClusterIdentityKinds() []string {
+	return slices.Clone(p.ClusterIdentityKinds)
+}
+
+func (p *YAMLProviderDefinition) CredentialPropagationFunc() func(
+	ctx context.Context,
+	cfg *credspropagation.PropagationCfg,
+	l logr.Logger,
+) (enabled bool, err error) {
+	return func(
+		ctx context.Context,
+		cfg *credspropagation.PropagationCfg,
+		l logr.Logger,
+	) (bool, error) {
+		l.Info(p.GetTitleName() + " creds propagation start")
+		return p.CredentialPropagation.Enabled,
+			credspropagation.PropagateProviderObjects(ctx, cfg, p.CredentialPropagation.Lua)
+	}
+}
+
+// RegisterFromYAML registers a provider from a YAML file
+func RegisterFromYAML(yamlFile string) error {
+	data, err := os.ReadFile(yamlFile)
+	if err != nil {
+		return fmt.Errorf("failed to read YAML file: %w", err)
+	}
+
+	var ypd YAMLProviderDefinition
+
+	if err := yaml.Unmarshal(data, &ypd); err != nil {
+		return fmt.Errorf("failed to unmarshal YAML: %w", err)
+	}
+
+	Register(&ypd)
+
+	return nil
+}
+
+// RegisterProvidersFromGlob loads and registers provider YAML files matching the glob pattern
+func RegisterProvidersFromGlob(pattern string) error {
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		return fmt.Errorf("failed to glob pattern %q: %w", pattern, err)
+	}
+
+	for _, file := range matches {
+		if err := RegisterFromYAML(file); err != nil {
+			return fmt.Errorf("provider %s: %w", filepath.Base(file), err)
+		}
+	}
+
+	return nil
+}