7984e612d8b57cb693effb3032d9b5c2fdc4b34c
Artem Bortnikov <brongineer747@gmail.com>
2025-05-09 17:38:04 +0300

    fix: remove ServiceSet type and fix StateManagementProviderList
    Signed-off-by: Artem Bortnikov <brongineer747@gmail.com>

--- api/v1beta1/statemanagementprovider_types.go
+++ api/v1beta1/statemanagementprovider_types.go
@@ -196,7 +196,7 @@ type StateManagementProvider struct {
 type StateManagementProviderList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []ServiceSet `json:"items"`
+	Items           []StateManagementProvider `json:"items"`
 }
 
 func init() {
