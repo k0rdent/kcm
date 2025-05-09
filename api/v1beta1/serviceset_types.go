6b8ff1fea1d547490096194a1767d00517a4c966
Artem Bortnikov <brongineer747@gmail.com>
2025-05-09 17:43:49 +0300

    feat: serviceset type bootstrap
    Signed-off-by: Artem Bortnikov <brongineer747@gmail.com>

---
+++ api/v1beta1/serviceset_types.go
@@ -0,0 +1,164 @@
+// Copyright 2024
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	ServiceSetKind = "ServiceSet"
+
+	DriftIgnorePatch = `- op: add
+  path: /metadata/annotations/projectsveltos.io~1driftDetectionIgnore
+  value: ok`
+)
+
+// ServiceSetSpec defines the desired state of ServiceSet
+type ServiceSetSpec struct {
+	// Config is the provider-specific configuration for the service set.
+	Config *apiextensionsv1.JSON `json:"config,omitempty"`
+
+	// Provider is the name of the provider to use to deploy services defined in the ServiceSet.
+	Provider string `json:"provider"`
+
+	// Cluster is the name of the ClusterDeployment
+	Cluster string `json:"cluster"`
+
+	// MultiClusterService is the name of the MultiClusterService
+	MultiClusterService string `json:"multiClusterService,omitempty"`
+
+	// TemplatesNamespace is the namespace where the ServiceTemplates  are deployed.
+	// Effectively it reflects the namespace of the [ClusterDeployment].
+	TemplatesNamespace string `json:"templatesNamespace"`
+
+	// Services is the list of services to deploy.
+	Services []ServiceWithValues `json:"services,omitempty"`
+}
+
+type ServiceWithValues struct {
+	// Name is the name of the service. If the ServiceTemplate is backed by Helm chart,
+	// then the name is the name of the Helm release.
+	Name string `json:"name"`
+
+	// Namespace is the namespace where the service is deployed. If the ServiceTemplate
+	// is backed by Helm chart, then the namespace is the namespace where the Helm release is deployed.
+	Namespace string `json:"namespace"`
+
+	// Template is the name of the ServiceTemplate to use to deploy the service.
+	Template string `json:"template"`
+
+	// Values is the values to pass to the ServiceTemplate.
+	Values string `json:"values,omitempty"`
+
+	// ValuesFrom is the list of sources of the values to pass to the ServiceTemplate.
+	ValuesFrom []ValuesFrom `json:"valuesFrom,omitempty"`
+}
+
+// ValuesFrom is the source of the values to pass to the ServiceTemplate. The source
+// can be a ConfigMap or a Secret located in the same namespace as the ServiceSet.
+type ValuesFrom struct {
+	// +kubebuilder:validation:Enum=ConfigMap;Secret
+
+	// Kind is the kind of the source.
+	Kind string `json:"kind"`
+
+	// Name is the name of the source.
+	Name string `json:"name"`
+}
+
+// ServiceSetStatus defines the observed state of ServiceSet
+type ServiceSetStatus struct {
+	// Deployed is true if the ServiceSet has been deployed
+	// +kubebuilder:default=false
+	Deployed bool `json:"deployed"`
+
+	// Provider is the state of the provider
+	Provider ProviderState `json:"provider,omitempty"`
+
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	// +listType=map
+	// +listMapKey=type
+
+	// Conditions is a list of conditions for the ServiceSet
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	// Services is a list of Service states in the ServiceSet
+	Services []ServiceState `json:"services,omitempty"`
+}
+
+// ProviderState is the state of the provider
+type ProviderState struct {
+	// Ready is true if the provider is ready
+	Ready bool `json:"ready,omitempty"`
+
+	// Suspended is true if the provider is suspended
+	Suspended bool `json:"suspended,omitempty"`
+}
+
+// ServiceState is the state of a Service
+type ServiceState struct {
+	// LastStateTransitionTime is the time the State was last transitioned
+	LastStateTransitionTime *metav1.Time `json:"lastStateTransitionTime"`
+
+	// Name is the name of the Service
+	Name string `json:"name"`
+
+	// Namespace is the namespace of the Service
+	Namespace string `json:"namespace"`
+
+	// Version is the version of the Service
+	Version string `json:"version"`
+
+	// State is the state of the Service
+	// +kubebuilder:validation:Enum=Deployed;Provisioning;Failed
+	State string `json:"state"`
+
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	// +listType=map
+	// +listMapKey=type
+
+	// Conditions is a list of conditions for the Service
+	Conditions []metav1.Condition `json:"conditions"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="age",type=date,JSONPath=`.metadata.creationTimestamp`,description="Time elapsed since object creation",priority=0
+
+// ServiceSet is the Schema for the servicesets API
+type ServiceSet struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ServiceSetSpec   `json:"spec,omitempty"`
+	Status ServiceSetStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ServiceSetList contains a list of ServiceSet
+type ServiceSetList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ServiceSet `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ServiceSet{}, &ServiceSetList{})
+}
