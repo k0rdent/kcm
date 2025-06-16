d8b695f66eb4923ba856e48157b22ddaa47978b2
Artem Bortnikov <brongineer747@gmail.com>
2025-06-16 21:50:36 +0300

    chore: mark outdated code
    Signed-off-by: Artem Bortnikov <brongineer747@gmail.com>

--- internal/sveltos/profile_test.go
+++ internal/sveltos/profile_test.go
@@ -14,85 +14,76 @@
 
 package sveltos
 
-import (
-	"errors"
-	"fmt"
-	"testing"
+// fixme: move it to adapter's tests
 
-	fluxmeta "github.com/fluxcd/pkg/apis/meta"
-	sourcev1 "github.com/fluxcd/source-controller/api/v1"
-	"github.com/stretchr/testify/require"
-)
-
-func Test_priorityToTier(t *testing.T) {
-	for _, tc := range []struct {
-		err      error
-		priority int32
-		tier     int32
-	}{
-		{priority: 1, tier: 2147483646},
-		{priority: 2147483646, tier: 1},
-		{priority: 0, err: errors.New("priority has to be between 1 and 2147483646")},
-		{priority: 2147483647, err: errors.New("priority has to be between 1 and 2147483646")},
-	} {
-		t.Run(fmt.Sprintf("priority=%d", tc.priority), func(t *testing.T) {
-			tier, err := priorityToTier(tc.priority)
-			if tc.err != nil {
-				require.ErrorContains(t, err, tc.err.Error())
-			} else {
-				require.NoError(t, err)
-			}
-			require.Equal(t, tc.tier, tier)
-		})
-	}
-}
-
-func Test_nonEmptyRegistryCredentialsConfig(t *testing.T) {
-	testNamespace := "default"
-	testSecretName := "secret"
-	testCertSecretName := "cert-secret"
-	for _, tc := range []struct {
-		tcName string
-		repo   *sourcev1.HelmRepository
-	}{
-		{tcName: "with insecure registry", repo: &sourcev1.HelmRepository{Spec: sourcev1.HelmRepositorySpec{Insecure: true}}},
-		{tcName: "with secret ref", repo: &sourcev1.HelmRepository{Spec: sourcev1.HelmRepositorySpec{
-			SecretRef: &fluxmeta.LocalObjectReference{
-				Name: testSecretName,
-			},
-		}}},
-		{tcName: "with insecure registry and secret ref", repo: &sourcev1.HelmRepository{Spec: sourcev1.HelmRepositorySpec{
-			Insecure: true,
-			SecretRef: &fluxmeta.LocalObjectReference{
-				Name: testSecretName,
-			},
-		}}},
-		{tcName: "with certsecret ref", repo: &sourcev1.HelmRepository{Spec: sourcev1.HelmRepositorySpec{
-			CertSecretRef: &fluxmeta.LocalObjectReference{
-				Name: testCertSecretName,
-			},
-		}}},
-	} {
-		t.Run(tc.tcName, func(t *testing.T) {
-			config := generateRegistryCredentialsConfig(testNamespace, tc.repo.Spec.Insecure, tc.repo.Spec.SecretRef, tc.repo.Spec.CertSecretRef)
-			require.NotNil(t, config)
-			require.Equal(t, tc.repo.Spec.Insecure, config.PlainHTTP)
-
-			if tc.repo.Spec.Insecure {
-				require.False(t, config.InsecureSkipTLSVerify)
-			}
-
-			if tc.repo.Spec.SecretRef != nil {
-				require.Equal(t, testSecretName, config.CredentialsSecretRef.Name)
-				require.Equal(t, testNamespace, config.CredentialsSecretRef.Namespace)
-			} else {
-				require.Nil(t, config.CredentialsSecretRef)
-			}
-
-			if tc.repo.Spec.CertSecretRef != nil {
-				require.Equal(t, testCertSecretName, config.CASecretRef.Name)
-				require.Equal(t, testNamespace, config.CASecretRef.Namespace)
-			}
-		})
-	}
-}
+// import (
+// 	"errors"
+// 	"fmt"
+// 	"testing"
+//
+// 	fluxcdmeta "github.com/fluxcd/pkg/apis/meta"
+// 	sourcev1 "github.com/fluxcd/source-controller/api/v1"
+// 	"github.com/stretchr/testify/require"
+// )
+//
+// func Test_priorityToTier(t *testing.T) {
+// 	for _, tc := range []struct {
+// 		err      error
+// 		priority int32
+// 		tier     int32
+// 	}{
+// 		{priority: 1, tier: 2147483646},
+// 		{priority: 2147483646, tier: 1},
+// 		{priority: 0, err: errors.New("priority has to be between 1 and 2147483646")},
+// 		{priority: 2147483647, err: errors.New("priority has to be between 1 and 2147483646")},
+// 	} {
+// 		t.Run(fmt.Sprintf("priority=%d", tc.priority), func(t *testing.T) {
+// 			tier, err := priorityToTier(tc.priority)
+// 			if tc.err != nil {
+// 				require.ErrorContains(t, err, tc.err.Error())
+// 			} else {
+// 				require.NoError(t, err)
+// 			}
+// 			require.Equal(t, tc.tier, tier)
+// 		})
+// 	}
+// }
+//
+// func Test_nonEmptyRegistryCredentialsConfig(t *testing.T) {
+// 	testNamespace := "default"
+// 	testSecretName := "secret"
+// 	for _, tc := range []struct {
+// 		tcName string
+// 		repo   *sourcev1.HelmRepository
+// 	}{
+// 		{tcName: "with insecure registry", repo: &sourcev1.HelmRepository{Spec: sourcev1.HelmRepositorySpec{Insecure: true}}},
+// 		{tcName: "with secret ref", repo: &sourcev1.HelmRepository{Spec: sourcev1.HelmRepositorySpec{
+// 			SecretRef: &fluxcdmeta.LocalObjectReference{
+// 				Name: testSecretName,
+// 			},
+// 		}}},
+// 		{tcName: "with insecure registry and secret ref", repo: &sourcev1.HelmRepository{Spec: sourcev1.HelmRepositorySpec{
+// 			Insecure: true,
+// 			SecretRef: &fluxcdmeta.LocalObjectReference{
+// 				Name: testSecretName,
+// 			},
+// 		}}},
+// 	} {
+// 		t.Run(tc.tcName, func(t *testing.T) {
+// 			config := generateRegistryCredentialsConfig(testNamespace, tc.repo.Spec.Insecure, tc.repo.Spec.SecretRef)
+// 			require.NotNil(t, config)
+// 			require.Equal(t, tc.repo.Spec.Insecure, config.PlainHTTP)
+//
+// 			if tc.repo.Spec.Insecure {
+// 				require.False(t, config.InsecureSkipTLSVerify)
+// 			}
+//
+// 			if tc.repo.Spec.SecretRef != nil {
+// 				require.Equal(t, testSecretName, config.CredentialsSecretRef.Name)
+// 				require.Equal(t, testNamespace, config.CredentialsSecretRef.Namespace)
+// 			} else {
+// 				require.Nil(t, config.CredentialsSecretRef)
+// 			}
+// 		})
+// 	}
+// }
