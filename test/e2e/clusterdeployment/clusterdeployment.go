ca8605627bb1840910d5c370f099a5cc4856f8a9
Bharath Nallapeta <bnallapeta@mirantis.com>
2025-09-12 12:05:15 +0530

    test(e2e): add openstack standalone and hosted control plane tests
    Signed-off-by: Bharath Nallapeta <bnallapeta@mirantis.com>

--- test/e2e/clusterdeployment/clusterdeployment.go
+++ test/e2e/clusterdeployment/clusterdeployment.go
@@ -40,12 +40,13 @@ import (
 type ProviderType string
 
 const (
-	ProviderCAPI    ProviderType = "cluster-api"
-	ProviderAWS     ProviderType = "infrastructure-aws"
-	ProviderAzure   ProviderType = "infrastructure-azure"
-	ProviderGCP     ProviderType = "infrastructure-gcp"
-	ProviderVSphere ProviderType = "infrastructure-vsphere"
-	ProviderAdopted ProviderType = "infrastructure-internal"
+	ProviderCAPI      ProviderType = "cluster-api"
+	ProviderAWS       ProviderType = "infrastructure-aws"
+	ProviderAzure     ProviderType = "infrastructure-azure"
+	ProviderGCP       ProviderType = "infrastructure-gcp"
+	ProviderOpenStack ProviderType = "infrastructure-openstack"
+	ProviderVSphere   ProviderType = "infrastructure-vsphere"
+	ProviderAdopted   ProviderType = "infrastructure-internal"
 )
 
 const KCMControllerLabel = "app.kubernetes.io/name=kcm"
@@ -77,6 +78,12 @@ var gcpHostedCPClusterDeploymentTemplateBytes []byte
 //go:embed resources/gcp-gke.yaml.tpl
 var gcpGkeClusterDeploymentTemplateBytes []byte
 
+//go:embed resources/openstack-standalone-cp.yaml.tpl
+var openstackStandaloneCPClusterDeploymentTemplateBytes []byte
+
+//go:embed resources/openstack-hosted-cp.yaml.tpl
+var openstackHostedCPClusterDeploymentTemplateBytes []byte
+
 //go:embed resources/vsphere-standalone-cp.yaml.tpl
 var vsphereStandaloneCPClusterDeploymentTemplateBytes []byte
 
@@ -95,6 +102,7 @@ func FilterAllProviders() []string {
 		GetProviderLabel(ProviderAWS),
 		GetProviderLabel(ProviderAzure),
 		GetProviderLabel(ProviderCAPI),
+		GetProviderLabel(ProviderOpenStack),
 		GetProviderLabel(ProviderVSphere),
 	}
 }
@@ -165,6 +173,10 @@ func Generate(templateType templates.Type, clusterName, template string) *kcmv1.
 		clusterDeploymentTemplateBytes = gcpStandaloneCPClusterDeploymentTemplateBytes
 	case templates.TemplateGCPGKE:
 		clusterDeploymentTemplateBytes = gcpGkeClusterDeploymentTemplateBytes
+	case templates.TemplateOpenStackStandaloneCP:
+		clusterDeploymentTemplateBytes = openstackStandaloneCPClusterDeploymentTemplateBytes
+	case templates.TemplateOpenStackHostedCP:
+		clusterDeploymentTemplateBytes = openstackHostedCPClusterDeploymentTemplateBytes
 	case templates.TemplateAdoptedCluster:
 		clusterDeploymentTemplateBytes = adoptedClusterDeploymentTemplateBytes
 	case templates.TemplateRemoteCluster:
